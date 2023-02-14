ba84dbf8de08927a4ecb60fabe73b9fcb8dfb374
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-14 23:32:10 +0600

    set componets for reuse and try font set and make theme components

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
+
+// import { useFonts, oswald_400Regular } from "@expo-google-font/inter";
+
+import { theme } from "./src/infrastructure/theme";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
