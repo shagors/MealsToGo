37c1575650d3859bc66f8eb0009deeff260549e3
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-15 10:32:43 +0600

    design and font sizes set and padding screens and install svg file

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,17 +2,31 @@ import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import { theme } from "../../../infrastructure/theme";
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: "#FFFFFF";
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
-  background-color: white;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: "#FFFFFF";
 `;
 const Title = styled(Text)`
-  padding: 16px;
+  font-size: 16px;
+  font-weight: 500;
+  padding: ${(props) => props.theme.space[3]};
+  color: "#262626";
+`;
+
+const Address = styled(Text)`
+  font-size: 16px;
+  font-weight: 400;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -30,7 +44,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
